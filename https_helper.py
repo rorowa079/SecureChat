733287532cb4dfb069295d51c1a2d9bd608d61fd
rorowa079 <105670146+rorowa079@users.noreply.github.com>
2026-05-08 11:56:09 -0700

    Delete https_helper.py

--- https_helper.py
+++
@@ -1,9 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl
-
-httpd = HTTPServer(("0.0.0.0", 8443), SimpleHTTPRequestHandler)
-ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ctx.load_cert_chain("certs/cert.pem", "certs/key.pem")
-httpd.socket = ctx.wrap_socket(httpd.socket, server_side=True)
-print("HTTPS helper running on https://0.0.0.0:8443")
-httpd.serve_forever()
